92a6fbc12a5fc48b687c82f33cd3735f87d17c5e
Jack Coventry <jackcoventry@gmail.com>
2026-01-08 14:30:49 +0000

    Imported default blog functionality for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
